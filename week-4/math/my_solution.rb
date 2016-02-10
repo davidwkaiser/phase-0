76744188a378296f1d87f5a65292be7839996479
davidwkaiser <davidwkaiser@yahoo.com>
2016-02-10 14:50:07 -0600

    add more stuff

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,22 @@
+# Math Methods
+
+# I worked on this challenge [by myself ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+  x=num_1+num_2
+end
+
+def subtract(num_1, num_2)
+  x = num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ x = num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ x = num_1 / num_2
+end
