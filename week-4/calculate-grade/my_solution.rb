417a49395cbf0ba1326b2cb7a05932e6663b379e
davidwkaiser <davidwkaiser@yahoo.com>
2016-02-11 14:02:13 -0600

    add letter grade

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,20 @@
+
+# Calculate a Grade
+
+# I worked on this challenge with: Daniel Homer
+
+
+# Your Solution Below
+def get_grade(num)
+  if num >= 90
+    'A'
+    elsif num >= 80
+      'B'
+    elsif num >= 70
+      'C'
+    elsif num >= 60
+      'D'
+    else
+      'F'
+  end
+end
