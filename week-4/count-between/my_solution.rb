bf26b2e1a8db70ba300cbef1514337b2458dd4cb
davidwkaiser <davidwkaiser@yahoo.com>
2016-02-12 23:59:15 -0600

    add the exercises

---
+++ week-4/count-between/my_solution.rb
@@ -0,0 +1,25 @@
+# Count Between
+
+# I worked on this challenge by myself.
+
+# count_between is a method with three arguments:
+#   1. An array of integers
+#   2. An integer lower bound
+#   3. An integer upper bound
+#
+# It returns the number of integers in the array between the lower and upper bounds,
+# including (potentially) those bounds.
+#
+# If +array+ is empty the method should return 0
+
+# Your Solution Below
+
+def count_between(list_of_integers, lower_bound, upper_bound)
+count = 0
+  list_of_integers.each do |number|
+    if number >= lower_bound && number <= upper_bound
+      count +=1
+    end
+  end
+  return count
+end
